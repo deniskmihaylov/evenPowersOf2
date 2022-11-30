95ed0f57883cb34c7414e1cf8cd481e597cc919c
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2022-11-30 02:07:54 +0000

    Add files via upload

---
+++ evenPowersOf2.js
@@ -0,0 +1,11 @@
+function solve (input) {
+    let n = Number(input[0]); 
+
+    for (i = 0; i <= n; i += 1) {
+        if(i % 2 === 0) {
+            console.log(Math.pow(2,i))
+        }
+    } 
+}
+
+solve ([10])
